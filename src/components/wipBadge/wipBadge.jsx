afae0d5dba735f3d8b1099ae3d0a12a2e9da349b
sarthakSrrrri <sarthaksrrrrivastava@gmail.com>
2026-08-30 06:55:28 +0530

    WIP

---
+++ src/components/wipBadge/wipBadge.jsx
@@ -0,0 +1,12 @@
+import './wipBadge.css'
+
+function WipBadge() {
+  return (
+    <div className="wip-badge">
+      <span className="wip-dot"></span>
+      🚧 Work in progress
+    </div>
+  )
+}
+
+export default WipBadge
